904f0527e11f60c027b8eec71a77b3188c330c6b
Dennis Koch <info@pixelarbeit.de>
2020-04-11 13:39:04 +0200

    feat: add tests fo batch file

--- move-wsl.bat
+++ move-wsl.bat
@@ -11,12 +11,17 @@ IF /I "%PROMPT%" NEQ "Y" GOTO END
 IF NOT EXIST "%WSL_TARGET%" (
     ECHO Creating target dir "%WSL_TARGET%" ...
     SETLOCAL enableextensions
-    MD %WSL_TARGET%
+    MD "%WSL_TARGET%"
     ENDLOCAL
 )
 
 ECHO Exporting VHDX to "%TEMP_FILE%" ...
 wsl --export %WSL_NAME% %TEMP_FILE%
+
+IF NOT EXIST "%TEMP_FILE%" (
+    ECHO "ERROR: Export failed. %TEMP_FILE% does not exist."
+    EXIT
+)
 
 ECHO Unregistering WSL ...
 wsl --unregister %WSL_NAME% >nul 2>&1
@@ -24,8 +29,13 @@ wsl --unregister %WSL_NAME% >nul 2>&1
 ECHO Importing "%WSL_NAME%" at %WSL_TARGET% ...
 wsl --import %WSL_NAME% %WSL_TARGET% %TEMP_FILE%
 
+IF NOT EXIST "%WSL_TARGET%/ext4.vhdx" (
+    ECHO "ERROR: Import failed. Target file not found. Export file at %TEMP_FILE%"
+    EXIT
+)
+
 ECHO Cleaning up ...
-DEL %TEMP_FILE%
+DEL "%TEMP_FILE%"
 
 ECHO Done!
 
