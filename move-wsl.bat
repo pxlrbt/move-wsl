759a4e7729338db79c00d632a79c338642437dd1
Dennis Koch <info@pixelarbeit.de>
2020-08-09 11:39:50 +0200

    fix: issues with paths with whitespaces in batch script

--- move-wsl.bat
+++ move-wsl.bat
@@ -1,12 +1,14 @@
 @echo off
+SETLOCAL
 
-SET WSL_NAME=%1
-SET WSL_TARGET=%2
+SET WSL_NAME=%~1
+SET WSL_TARGET=%~2
+
+IF %WSL_TARGET:~-1%==\ SET WSL_TARGET=%WSL_TARGET:~0,-1%
 SET TEMP_FILE=%WSL_TARGET%\%WSL_NAME%.tar
 
-SET /P PROMPT= "Move WSL "%WSL_NAME%" to %WSL_TARGET%? (Y|n) "
-
-IF /I "%PROMPT%" NEQ "Y" GOTO END
+SET /P PROMPT="Move WSL '%WSL_NAME%' to '%WSL_TARGET%'? (Y|n) "
+IF /I "%PROMPT%" NEQ "Y" GOTO :EOF
 
 IF NOT EXIST "%WSL_TARGET%" (
     ECHO Creating target dir "%WSL_TARGET%" ...
@@ -16,39 +18,33 @@ IF NOT EXIST "%WSL_TARGET%" (
 )
 
 ECHO Exporting VHDX to "%TEMP_FILE%" ...
-wsl --export %WSL_NAME% %TEMP_FILE%
+wsl --export %WSL_NAME% "%TEMP_FILE%"
 
 IF %ERRORLEVEL% EQU 0 IF EXIST "%TEMP_FILE%" (
+    ECHO Export successful.
     GOTO :UNREGISTER
 )
-ECHO ERROR: Export failed
+ECHO ERROR: Export failed!
 CALL :CLEANUP
-EXIT
+GOTO :EOF
 
 :UNREGISTER
 ECHO Unregistering WSL ...
 wsl --unregister %WSL_NAME% >nul 2>&1
 
-ECHO Importing "%WSL_NAME%" at %WSL_TARGET% ...
-wsl --import %WSL_NAME% %WSL_TARGET% %TEMP_FILE%
+ECHO Importing "%WSL_NAME%" at "%WSL_TARGET%" ...
+wsl --import "%WSL_NAME%" "%WSL_TARGET%" "%TEMP_FILE%"
 
-
-IF NOT EXIST "%WSL_TARGET%/ext4.vhdx"  (
-    IF NOT EXIST "%WSL_TARGET%/rootfs"  (
-        ECHO "ERROR: Import failed. Target file not found. Export file at %TEMP_FILE%"
-        EXIT
+ECHO Validating import ...
+IF NOT EXIST "%WSL_TARGET%\ext4.vhdx"  (
+    IF NOT EXIST "%WSL_TARGET%\rootfs"  (
+        ECHO ERROR: Import failed! Target file not found. Export file at '%TEMP_FILE%'
+        GOTO :EOF
     )
 )
-
-CALL :CLEANUP
-
-ECHO Done!
-
-:END
-PAUSE
-EXIT
 
 :CLEANUP
 ECHO Cleaning up ...
 IF EXIST "%TEMP_FILE%" DEL "%TEMP_FILE%"
-GOTO :EOF
+
+ECHO Done!
